f9a58ee21d9c328dddcc47e2acb81cd2eb293942
Umar1311829 <star_umar2008@hotmail.com>
2022-03-30 16:03:31 +0100

    added import

--- VeterinarySurgery/src/main/java/serialisation/Serialisation.java
+++ VeterinarySurgery/src/main/java/serialisation/Serialisation.java
@@ -1,13 +1,14 @@
 package serialisation;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
+
+import java.io.*;
+//import java.io.FileOutputStream;
+//import java.io.IOException;
+//import java.io.ObjectOutputStream;
 
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-
 /**
  *
  * @author star_
@@ -19,18 +20,17 @@ public class Serialisation {
      */
     public static void main(String[] args) {
         Animal myAnimal = new Animal("Polly", "Grey", "German", 10, "Umar", "Abid");
-        
+
         try {
-            FileOutputStream fileOut = new FileOutputStream ("./surgery.txt");
+            FileOutputStream fileOut = new FileOutputStream("./surgery.txt");
             ObjectOutputStream out = new ObjectOutputStream(fileOut);
             out.writeObject(myAnimal);
             out.close();
             fileOut.close();
             System.out.printf("Serialized data is saved in ./surgery.txt file");
-        }
-        catch (IOException myException) {
+        } catch (IOException myException) {
             myException.printStackTrace();
-            }
+        }
     }
-    
+
 }
