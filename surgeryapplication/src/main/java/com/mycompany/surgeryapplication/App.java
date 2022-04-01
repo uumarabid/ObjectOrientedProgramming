5f71dc7a0cd982f23c0e8443301e9fbbd4b2ec97
Umar1311829 <star_umar2008@hotmail.com>
2022-04-01 17:14:18 +0100

    test

--- surgeryapplication/src/main/java/com/mycompany/surgeryapplication/App.java
+++ surgeryapplication/src/main/java/com/mycompany/surgeryapplication/App.java
@@ -49,16 +49,11 @@ public class App extends Application {
     
     
 
-//    @Override
-//    public void start(Stage stage) {
-//        var javaVersion = SystemInfo.javaVersion();
-//        var javafxVersion = SystemInfo.javafxVersion();
-//
-//        var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-//        var scene = new Scene(new StackPane(label), 640, 480);
-//        stage.setScene(scene);
-//        stage.show();
-//    }
+    @Override
+    public void start(Stage stage) {
+        stage.setScene(scene);
+        stage.show();
+    }
 
     public static void main(String[] args) {
         launch();
