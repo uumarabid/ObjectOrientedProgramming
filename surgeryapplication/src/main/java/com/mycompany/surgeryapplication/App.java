dca83020d8a1ddcacd561e666d4c773cb77e52f1
Umar1311829 <star_umar2008@hotmail.com>
2022-04-01 17:31:47 +0100

    horizontal boxes created

--- surgeryapplication/src/main/java/com/mycompany/surgeryapplication/App.java
+++ surgeryapplication/src/main/java/com/mycompany/surgeryapplication/App.java
@@ -7,17 +7,17 @@ import javafx.scene.control.Label;
 import javafx.scene.control.Separator;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-
 
 /**
  * JavaFX App
  */
 public class App extends Application {
-    
+
     private StoreList animalList;
-    
+
     // WIDTH and HEIGHT of GUI
     private final int WIDTH = 1000;
     private final int HEIGHT = 800;
@@ -25,32 +25,36 @@ public class App extends Application {
     private Label headingLabel = new Label("Book in your animal"); // main heading
     private Label animalleLabel = new Label("Animal details"); // sub heading
     private Label nameLabel = new Label("Name");
-    private TextField nameField =  new TextField();
+    private TextField nameField = new TextField();
     private Label colourLabel = new Label("Colour");
-    private TextField colourField =  new TextField();
+    private TextField colourField = new TextField();
     private Label genderLabel = new Label("Gender");
-    private TextField genderField =  new TextField();
+    private TextField genderField = new TextField();
     private Label ageLabel = new Label("Age");
-    private TextField ageField =  new TextField();
+    private TextField ageField = new TextField();
     private Label illnessesLabel = new Label("Illnesses");
-    private TextField illnessesField =  new TextField();
+    private TextField illnessesField = new TextField();
     private Separator sectSeparator = new Separator();
     private Separator sectSeparator2 = new Separator();
     private Separator sectSeparator3 = new Separator();
     private Separator sectSeparator4 = new Separator();
     private Label ownerLabel = new Label("Owner Details"); // sub heading
     private Label giveNameLabel = new Label("Given Name");
-    private TextField giveNameField =  new TextField();
+    private TextField giveNameField = new TextField();
     private Label surnameLabel = new Label("Surname");
-    private TextField surnameField =  new TextField();
-    private TextArea displayAnimals  = new TextArea();
+    private TextField surnameField = new TextField();
+    private TextArea displayAnimals = new TextArea();
     private Button addButton = new Button("Book in Animal");
-    
-    
-    
 
     @Override
     public void start(Stage stage) {
+
+        animalList = new StoreList(20);
+        //horizontal boxes
+        HBox animalDetails = new HBox(8); // spacing = 8
+        HBox ownerDetails = new HBox(8);
+        
+
         stage.setScene(scene);
         stage.show();
     }
